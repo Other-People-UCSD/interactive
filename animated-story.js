944b293fabfb0e8e08016c6f034e025d588b3a06
Kevin-Jjang <k1jang@ucsd.edu>
2023-02-07 22:56:27 -0800

    Missed Connections draft 1

--- animated-story.js
+++ animated-story.js
@@ -49,20 +49,78 @@ function beginStory() {
  * @returns 0 on success
  */
 async function story() {
-    const one    = await writeTextChar(1, 20);
-    const two    = await writeTextChar(2, 100);
-    const del1   = await delTextChar(1, 80);
-    // const del0   = await delTextWord(1, 500);
-    // const del3   = await delTextChar(2, 20);
-    const three  = await writeTextChar(3, 10);
-    const four   = await writeTextChar(4, 20);
-    const five   = await writeTextChar(5, 25);
-    const six    = await writeTextChar(6, 30);
-    const seven  = await writeTextChar(7, 10);
-    const eight  = await writeTextChar(8, 10);
-    const nine   = await writeTextChar(9, 20);
-    const ten    = await writeTextChar(10, 20);
-    const eleven = await writeTextChar(11, 20);
+    const shortPause = 1000*1;
+    const longPause = 1000*3;
+    const normalType = 40;
+    const slowType = normalType*2;
+    const slowerType = normalType*4;
+
+
+    const w1    = await writeTextChar(1, normalType, shortPause);
+    const w2    = await writeTextChar(2, normalType, longPause);
+    const w3    = await writeTextChar(3, normalType, shortPause);
+
+    const w21   = await writeTextChar(21, slowType, shortPause);
+    const d3    = await delTextChar(3, 50);
+    const d2    = await delTextChar(2, 50);
+    const d1    = await delTextChar(1, 50);
+    const d21   = await delTextChar(21, 50, shortPause);
+    
+    const w31    = await writeTextChar(31, normalType, shortPause);
+    const d31    = await delTextChar(31, 50, longPause);
+    
+    const w41    = await writeTextChar(41, slowType, shortPause);
+    const w42    = await writeTextChar(42, normalType, shortPause);
+    const w43    = await writeTextChar(43, normalType, shortPause);
+    const w44    = await writeTextChar(44, slowType, shortPause);
+    
+    const w51    = await writeTextChar(51, normalType, shortPause);
+    const w52    = await writeTextChar(52, normalType);
+    const w53    = await writeTextChar(53, slowerType);
+    const w54    = await writeTextChar(54, slowType);
+    const w55    = await writeTextChar(55, normalType);
+
+    const w61    = await writeTextChar(61, normalType, longPause);
+    const w62    = await writeTextChar(62, normalType);
+
+    const w71    = await writeTextChar(71, slowType, longPause);
+    
+    const w81    = await writeTextChar(81, slowType, shortPause);
+    const w82    = await writeTextChar(82, normalType, longPause);
+    const w83    = await writeTextChar(83, normalType, shortPause);
+    const w84    = await writeTextChar(84, normalType, shortPause);
+    const w85    = await writeTextChar(85, normalType);
+    const w86    = await writeTextChar(86, slowerType);
+    const w87    = await writeTextChar(87, normalType);
+    const w88    = await writeTextChar(88, slowType, shortPause);
+    const w89    = await writeTextChar(89, normalType);
+
+    const w91    = await writeTextChar(91, normalType, shortPause);
+    const w92    = await writeTextChar(92, normalType, shortPause);
+    const w93    = await writeTextChar(93, normalType);
+    const w94    = await writeTextChar(94, slowType);
+    const w95    = await writeTextChar(95, normalType, shortPause);
+    const w96    = await writeTextChar(96, normalType);
+
+    const w101   = await writeTextChar(101, normalType);
+    const w102   = await writeTextChar(102, slowerType);
+    const w103   = await writeTextChar(103, normalType, shortPause);
+    const w104   = await writeTextChar(104, normalType, shortPause);
+    const w105   = await writeTextChar(105, normalType, shortPause);
+    const w106   = await writeTextChar(106, normalType);
+    const w107   = await writeTextChar(107, slowType, longPause);
+    const w108   = await writeTextChar(108, slowType, shortPause);
+    const w109   = await writeTextChar(109, slowType);
+
+    const w111   = await writeTextChar(111, normalType, longPause);
+
+    const delOut = await delPassage('output-text', shortPause);
+
+    const w121   = await writeTextChar(121, slowType);
+    const d121   = await delTextChar(121, slowerType);
+
+    const final = document.getElementById('final');
+    final.classList.remove('hidden');
     return 0;
 }
 
@@ -70,9 +128,10 @@ async function story() {
  * Writes the target text character by character
  * @param {int} id    The id of the text area to write
  * @param {int} rate  The speed in milliseconds that characters are written
+ * @param {int} delay The delay, in milliseconds, before the function finally ends
  * @returns {Promise} Announces the function has finished writing the targeted text 
  */
-function writeTextChar(id, rate) {
+function writeTextChar(id, rate, delay) {
     return new Promise((resolve) => {
         // Get the parent block to append the target HTML element to
         let outBlock;
@@ -89,7 +148,7 @@ function writeTextChar(id, rate) {
         else // If this is a line inside a paragraph, the parent is defined in the original story
         {
             let refId = ref.parentElement.id;
-            refId = parseInt(refId.match(/\d/g));
+            refId = parseInt(refId.match(/\d+/g));
             outBlock = document.getElementById(refId + ref.parentElement.nodeName);
             // Paragraph does not exist, create the block
             if (outBlock == null) {
@@ -116,7 +175,7 @@ function writeTextChar(id, rate) {
 
             if (i >= refText.length) {
                 clearInterval(outStream);
-                return resolve(id);
+                setTimeout(() => { return resolve(id)}, delay);
             }
         }, rate);
     });
@@ -158,7 +217,7 @@ function checkTag(input) {
  * @param {int} rate  The speed in milliseconds that characters are deleted
  * @returns {Promise} Announces the function has finished deleting the targeted text 
  */
-function delTextChar(id, rate) {
+function delTextChar(id, rate, delay) {
     return new Promise((resolve) => {
         const outText = document.getElementById(id + 'o');
         const text = document.getElementById(id).innerText;
@@ -174,8 +233,13 @@ function delTextChar(id, rate) {
             i -= 1;
             if (i < 0) {
                 clearInterval(outStream);
+                const outParent = outText.parentElement;
                 outText.remove();
-                return resolve(id);
+                console.log(outParent.id);
+                if (outParent.children.length == 0) {
+                    outParent.remove();
+                }
+                setTimeout(() => { return resolve(id) }, delay);
             }
         }, rate);
     })
@@ -226,5 +290,22 @@ function delTextWord(id, rate) {
                 return resolve(id);
             }
         }, rate);
-    })
+    });
+}
+
+function delPassage(id, delay) {
+    return new Promise((resolve) => {
+        const passage = document.getElementById(id);
+        passage.style.backgroundColor = '#ffb7b7';
+        setTimeout(() => { 
+            if (id == 'output-text') {
+                passage.innerHTML = '';
+                passage.style.backgroundColor = null;
+
+            } else {
+                passage.remove();
+            }
+            return resolve(id);
+        }, delay);
+    });
 }
